ea65b51f5340ca1f10406bce50fa4092d08657bb
guinhomoraes <guilhermeguilherme98@gmail.com>
2018-06-07 14:17:11 -0300

    criação das views

---
+++ sistemaescolar/resources/views/Aluno/cadastroAluno.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+	
+</head>
+<body>
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-sm-3"></div>
+			<div class="col-sm-6"></div>
+			<div class="col-sm-3"></div>
+		</div>
+	</div>
+</body>
+</html>
